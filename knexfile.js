3b4c6a259bad4f596d9865d6263f0024521d67c3
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2019-03-01 17:14:50 -0700

    tests

--- knexfile.js
+++ knexfile.js
@@ -7,36 +7,10 @@ module.exports = {
     connection: 'postgres://localhost/sports'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
+
